74329891b580990b3b148dfe5b6cd9226a090b74
Abhishek <abhidp55@gmail.com>
2023-09-07 13:37:47 +0530

    added a navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,42 @@
 import React, { useState } from "react";
 
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div class="container">
+      <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              About
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Skills
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Projects
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Work Exp
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
 }
