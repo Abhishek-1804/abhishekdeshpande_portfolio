96c445f3bca2a32fef898702ecb62b93b35a366d
Abhishek <abhidp55@gmail.com>
2023-09-08 11:48:14 +0530

    navbar select and scroll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Navbar from "./Navbar/Navbar";
 import Home from "./Home/Home";
 import Skills from "./Skills/Skills";
@@ -7,14 +7,59 @@ import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
 
 export default function App() {
+  // Create refs for each section
+  const homeRef = useRef(null);
+  const skillsRef = useRef(null);
+  const workExpRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  // Function to scroll to a section
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // Function to handle navigation link clicks
+  const handleNavLinkClick = (sectionName) => {
+    switch (sectionName) {
+      case "home":
+        scrollToSection(homeRef);
+        break;
+      case "skills":
+        scrollToSection(skillsRef);
+        break;
+      case "work-exp":
+        scrollToSection(workExpRef);
+        break;
+      case "projects":
+        scrollToSection(projectsRef);
+        break;
+      case "contact":
+        scrollToSection(contactRef);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
-      <Navbar />
-      <Home />
-      <Skills />
-      <Work />
-      <Projects />
-      <Contact />
+      <Navbar handleNavLinkClick={handleNavLinkClick} />
+      <section ref={homeRef} id="home" className="section-with-padding">
+        <Home />
+      </section>
+      <section ref={skillsRef} id="skills" className="section-with-padding">
+        <Skills />
+      </section>
+      <section ref={workExpRef} id="work-exp" className="section-with-padding">
+        <Work />
+      </section>
+      <section ref={projectsRef} id="projects" className="section-with-padding">
+        <Projects />
+      </section>
+      <section ref={contactRef} id="contact" className="section-with-padding">
+        <Contact />
+      </section>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,77 @@
 import React, { useState } from "react";
+import "./Navbar.css";
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const [active, setActive] = useState("home");
+
+  const { handleNavLinkClick } = props;
+
+  const handleNavLinkClickAndUpdateActive = (sectionName) => {
+    setActive(sectionName);
+    handleNavLinkClick(sectionName);
+  };
+
   return (
-    <div class="container">
-      <header class="d-flex justify-content-center py-3">
-        <ul class="nav nav-pills">
-          <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page">
-              Home
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              Skills
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              Work Exp
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              Projects
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </header>
-    </div>
+    <section className="navbar">
+      <div className="d-flex container align-items-center justify-content-center">
+        <header className="d-flex justify-content-center py-3 top-0">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <a
+                href="#home"
+                className={`nav-link mx-2 ${active === "home" ? "active" : ""}`}
+                onClick={() => handleNavLinkClickAndUpdateActive("home")}
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#skills"
+                className={`nav-link mx-2 ${
+                  active === "skills" ? "active" : ""
+                }`}
+                onClick={() => handleNavLinkClickAndUpdateActive("skills")}
+              >
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#work-exp"
+                className={`nav-link mx-2 ${
+                  active === "work-exp" ? "active" : ""
+                }`}
+                onClick={() => handleNavLinkClickAndUpdateActive("work-exp")}
+              >
+                Work Exp
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#projects"
+                className={`nav-link mx-2 ${
+                  active === "projects" ? "active" : ""
+                }`}
+                onClick={() => handleNavLinkClickAndUpdateActive("projects")}
+              >
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                className={`nav-link mx-2 ${
+                  active === "contact" ? "active" : ""
+                }`}
+                onClick={() => handleNavLinkClickAndUpdateActive("contact")}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
+    </section>
   );
 }
