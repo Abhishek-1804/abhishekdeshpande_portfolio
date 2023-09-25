5712b36aca6e9bf771e292374d97fc89eb23b73c
Abhishek <abhidp55@gmail.com>
2023-09-25 16:16:02 +0530

    updated resume

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home(props) {
   const hireMeEmail = "abhidp55@gmail.com";
 
   const pdfUrl =
-    "https://drive.google.com/file/d/1g6K5eHnPP-WTj0ofTAwbUPTF5rwK4-bt/view?usp=sharing";
+    "https://drive.google.com/file/d/1angGjjhrL2_X98xlY9tUwPr9_1uj1HIN/view?usp=sharing";
 
   return (
     <section className={`home-section ${props.darkMode && "dark-mode"}`}>
