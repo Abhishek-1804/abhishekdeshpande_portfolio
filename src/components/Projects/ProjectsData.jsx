4e757c934d7aa594429f02d32ec955415e11e259
Abhishek <abhidp55@gmail.com>
2024-06-04 15:44:32 -0400

    minor updates

--- src/components/Projects/ProjectsData.jsx
+++ src/components/Projects/ProjectsData.jsx
@@ -3,20 +3,20 @@ const ProjectsData = [
     name: "Cotton Disease Classifier",
     link: "https://github.com/Abhishek-1804/Cotton_disease_classification",
     description: "A machine learning model to classify cotton diseases",
-    tools: ["Python", "TensorFlow", "Keras"],
+    tools: ["Python", "TensorFlow", "CNN"],
   },
   {
     name: "Deep Q Learning Snake Game",
     link: "https://github.com/Abhishek-1804/SnakeAI",
     description: "An AI-based snake game using Deep Q Learning",
-    tools: ["Python", "PyTorch", "OpenAI Gym"],
+    tools: ["Python", "PyTorch", "Reinforcement Learning"],
   },
   {
     name: "Notion Notes App",
     link: "https://github.com/Abhishek-1804/notion_notes_app",
     description:
       "A notes app integrated with Notion and Siri Shortcuts hosted on Firebase",
-    tools: ["JavaScript", "Node.js", "Notion API"],
+    tools: ["JavaScript", "Node.js", "Notion API", "Firebase"],
   },
 ];
 
